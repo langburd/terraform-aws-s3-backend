1e2e4bd48c9802bf09d0ce1a3314bce6ef1befee
Avi Langburd <avi@langburd.com>
2021-08-11 00:33:01 +0300

    add output "s3_bucket_region"

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "s3_bucket" {
   value = aws_s3_bucket.terraform_state_s3_bucket.bucket
 }
 
+output "s3_bucket_region" {
+  value = aws_s3_bucket.terraform_state_s3_bucket.region
+}
+
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.terraform_state_locking_dynamodb.name
 }
